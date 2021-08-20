7f4911e8f1a426b471fce80a9511bfb4ad7ea9f8
Darkenn <obisson21@gmail.com>
2021-08-20 19:33:41 +0200

    pretty

--- tests/GetOHLC.py
+++ tests/GetOHLC.py
@@ -1,5 +1,5 @@
 from pydefistats.chains.bsc import ChainInfo as BSCInfo 
 from pydefistats.dex_stats import getOHLC
-
+import json
 ohlc = getOHLC(BSCInfo().network, "Pancake v2", "0x64d5baf5ac030e2b7c435add967f787ae94d0205", "0xe9e7cea3dedca5984780bafc599bd69add087d56", 100)
-print(ohlc)
+print(json.dumps(ohlc,indent=2))
