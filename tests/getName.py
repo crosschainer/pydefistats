dc278210e12dcbbc4e18d375e1bff0ed788813b2
crosschainer <68580992+crosschainer@users.noreply.github.com>
2021-08-20 08:28:55 +0200

    Create getName Test

---
+++ tests/getName.py
@@ -0,0 +1,5 @@
+from pydefistats.chains.bsc import ChainInfo as BSCInfo 
+from pydefistats.chain_stats import getName
+
+Name = getName(BSCInfo().network, "0x64d5baf5ac030e2b7c435add967f787ae94d0205")
+print(Name)
