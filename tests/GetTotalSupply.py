37405543f12b80470b4183502529be84040394c1
crosschainer <68580992+crosschainer@users.noreply.github.com>
2021-08-20 07:49:35 +0200

    Update GetTotalSupply.py

--- tests/GetTotalSupply.py
+++ tests/GetTotalSupply.py
@@ -1,5 +1,5 @@
-from pydefistats.chains.bsc import ChainInfo 
+from pydefistats.chains.bsc import ChainInfo as BSCInfo 
 from pydefistats.chain_stats import getTotalSupply
 
-TotalSupply = getTotalSupply(ChainInfo().network, "0x64d5baf5ac030e2b7c435add967f787ae94d0205")
+TotalSupply = getTotalSupply(BSCInfo().network, "0x64d5baf5ac030e2b7c435add967f787ae94d0205")
 print(TotalSupply)
